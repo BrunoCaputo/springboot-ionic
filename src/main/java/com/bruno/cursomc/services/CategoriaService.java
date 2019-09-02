5f801c01c681d52d111f88d792fc770c152d69bb
Bruno Caputo <bruno.caputo@leucotron.com.br>
2019-09-02 10:00:44 -0300

    first commit

---
+++ src/main/java/com/bruno/cursomc/services/CategoriaService.java
@@ -0,0 +1,69 @@
+package com.bruno.cursomc.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+
+import com.bruno.cursomc.domain.Categoria;
+import com.bruno.cursomc.dto.CategoriaDTO;
+import com.bruno.cursomc.repositories.CategoriaRepository;
+import com.bruno.cursomc.services.exceptions.DataIntegrityException;
+import com.bruno.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+
+	public Categoria find(Integer id) {
+		Categoria obj = repo.findOne(id);
+		if (obj == null) {
+			throw new ObjectNotFoundException(
+					"Objeto não encontrado! ID: " + id + ", Tipo: " + Categoria.class.getName());
+		}
+		return obj;
+	}
+
+	public Categoria insert(Categoria obj) {
+		Categoria newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+
+	public Categoria update(Categoria obj) {
+		find(obj.getId());
+		return repo.save(obj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.delete(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos!");
+		}
+	}
+
+	public List<Categoria> findAll() {
+		return repo.findAll();
+	}
+
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
+	}
+	
+	private void updateData(Categoria newObj, Categoria obj) {
+		newObj.setNome(obj.getNome());
+	}
+}
---
+++ src/main/java/com/bruno/cursomc/services/PedidoService.java
@@ -0,0 +1,23 @@
+package com.bruno.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bruno.cursomc.domain.Pedido;
+import com.bruno.cursomc.repositories.PedidoRepository;
+import com.bruno.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+
+	public Pedido find(Integer id) {
+		Pedido obj = repo.findOne(id);
+		if (obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! ID: " + id + ", Tipo: " + Pedido.class.getName());
+		}
+		return obj;
+	}
+}
